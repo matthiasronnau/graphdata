e47621c13dd1c246acb5d0a2847dbeb39dce52be
matthasronnau@berkeley.edu <matthiasronnau@berkeley.edu>
2021-03-12 22:47:10 -0800

    Add testing function

--- R/graphdata.R
+++ R/graphdata.R
@@ -1,5 +1,5 @@
 #' @title Graph Data
-#' @description Creates an object of class \code{"iGraph"} by transforming line order data into network data.
+#' @description Creates an object of class \code{"iGraph"} by transforming event level data into network data.
 #' @param data dataframe of purchase history
 #' @param id column name identifying the user id
 #' @param product_id column name identifying the id of the objects purchased
@@ -9,6 +9,8 @@
 #' @export
 
 graph_data <- function(data = df, id = "user_id", product_id = "product_id") {
+  check_inputs(data, id, product_id)
+
   data_no_missing <- na.omit(data)
 
   grouped <- dplyr::group_by(data_no_missing, dplyr::across(id))
@@ -43,5 +45,20 @@ graph_data <- function(data = df, id = "user_id", product_id = "product_id") {
   g = igraph::make_graph(verts)
   g
 }
+
+#Ensure inputs are specified
+check_inputs <- function(data, id, product_id) {
+  if(missing(data) | is.null(data)){
+    stop("\n'data' must be a dataframe")
+  }
+  if(missing(id) | is.null(id)){
+    stop("\n'id' must be a vector of user ids")
+  }
+  if(missing(product_id) | is.null(product_id)){
+    stop("\n'product_id' myst be a vector of product ids")
+  }
+  TRUE
+}
+
 
 
